a80cc53f7fcfc413886883e353ac105418ff4905
David Folkins <david.folkins88@gmail.com>
2020-09-24 16:09:49 -0400

    added the ability to get latlong by ip

--- iss.js
+++ iss.js
@@ -1,14 +1,4 @@
-/**
- * Makes a single API request to retrieve the user's IP address.
- * Input:
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
- */
-
 const request = require('request');
-
 
 const fetchMyIP = function(callback) {
   const url = 'https://api.ipify.org?format=json';
@@ -30,8 +20,33 @@ const fetchMyIP = function(callback) {
 
     const ip = JSON.parse(body).ip;
     callback(null, ip);
+    fetchCoordsByIP(ip, callback);
   });
 };
 
 
-module.exports = { fetchMyIP };
+
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://ipvigilante.com/json/${ip}`, (error, response, body) => {
+    // error can be set if invalid domain, user is offline, etc.
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    
+    const lat = JSON.parse(body).data.latitude;
+    const long = JSON.parse(body).data.longitude;
+    const latLong = {latitude: lat, longitude: long};
+
+    callback(null, latLong);
+  });
+};
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
